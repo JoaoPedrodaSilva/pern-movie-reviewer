8f8bb42cf2fbc267b139fba95ac5acf45b6bcb86
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-07-12 18:32:02 -0300

    fixing enviroment

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://joaopedro-pern-todo-list.herokuapp.com/api/todos'
+    baseURL: 'https://joaopedro-pern-movie-reviewer.herokuapp.com/api/movies'
 })
