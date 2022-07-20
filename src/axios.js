7fac068e4e6e53b82db3104f11c35ebd285e01b5
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-07-19 21:50:20 -0300

    hosting remotely

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export default axios.create({
-    baseURL: 'http://localhost:3000/api/movies'
-})
-
 // export default axios.create({
-//     baseURL: 'https://joaopedro-pern-movie-reviewer.herokuapp.com/api/movies'
+//     baseURL: 'http://localhost:3000/api/movies'
 // })
+
+export default axios.create({
+    baseURL: 'https://joaopedro-pern-movie-reviewer.herokuapp.com/api/movies'
+})
